c005692bd74c09daa6cd734384606e5b2b3b9f35
Hiram <147564077+Hiram10tec@users.noreply.github.com>
2024-11-21 11:15:31 -0600

    Feature Consult Historical Data

---
+++ TC2007B_Examen/TC2007B_Examen/Data/Models/Historical/Historical.swift
@@ -0,0 +1,25 @@
+//
+//  Historical.swift
+//  ExamenTC2007B
+//
+//  Created by Hiram Mendoza  on 21/11/24.
+//
+
+
+import Foundation
+
+
+import Foundation
+
+struct HistoricalData: Codable {
+    let date: String
+    let description: String
+    let lang: String
+    let category1: String
+    let category2: String
+    let granularity: String
+    let createdAt: String
+    let updatedAt: String
+    let objectId: String
+    let className: String
+}
---
+++ TC2007B_Examen/TC2007B_Examen/Data/Models/Historical/HistoricalRequiriment.swift
@@ -0,0 +1,13 @@
+//
+//  HistoricalRequiriment.swift
+//  ExamenTC2007B
+//
+//  Created by Hiram Mendoza  on 21/11/24.
+//
+
+import Foundation
+
+/// Defines the parameters required for fetching historical data.
+struct FetchHistoricalDataRequirement {
+    let page: Int
+}
